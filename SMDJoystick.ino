e1c1c43c056bd4caa210f1f6a0ab4dd6415271f8
Ivan Naumov <naumov5550123@gmail.com>
2018-01-17 23:11:35 +0300

    new file:   SMDJoystick.ino 	new file:   SMDjoystick.cpp 	new file:   SMDjoystick.h 	deleted:    SEGACOM.ino 	deleted:    lib/SMDjoystick.cpp 	deleted:    lib/SMDjoystick.h

---
+++ SMDJoystick.ino
@@ -0,0 +1,55 @@
+#include "SMDjoystick.h"
+
+SMDjoystick first(A0, A1, A2, A3, A4, A5, 9);
+SMDjoystick second(2, 3, 4, 5, 6, 7, 8);
+int data;
+bool isRunning = false;
+
+void setup() {
+  Serial.begin(19200);
+}
+
+void loop() {
+  if (isRunning) {
+    String out = String(first.read(1)) 
+    + " " 
+    + String(second.read(2));
+    Serial.println(out);
+    //Serial.flush();
+  }
+  
+  delay(10);
+}
+
+void serialEvent() {
+    data = 0;
+  while (Serial.available() > 0) {
+    data = Serial.parseInt();
+  }
+
+  switch (data) {
+    case 222:
+      //echo beacon
+      Serial.println("sega_ready");
+      break;
+
+    case 444:
+      //start
+      isRunning = true;
+      Serial.println("started");
+      break;
+
+    case 666:
+      //stop
+      isRunning = false;
+      Serial.println("stopped");
+      break;
+
+    default:
+      //something else
+      break;
+  }
+
+  Serial.flush();
+}
+
